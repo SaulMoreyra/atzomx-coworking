1c38294b700429c72ca26f643c46ab40e262377a
SaulMoreyra <armhacam21@gmail.com>
2025-03-05 13:01:47 -0600

    feat: add new distributon for footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,30 @@
 "use client";
-
-import { useTranslations } from "next-intl";
-import Link from "next/link";
 import React from "react";
+import { useTranslations } from "next-intl";
 import { Facebook, Instagram, Phone } from "react-feather";
 
-import { Visa, Mastercard } from "react-payment-logos/dist/flat";
+import { Mastercard, Visa } from "react-payment-logos/dist/flat";
+
+const ADDRESS =
+  "C. de Los Libres 800-letra B, RUTA INDEPENDENCIA, Centro, 68000 Oaxaca de Juárez, Oax.";
+
+const SOCIAL_NETWORKS = [
+  {
+    icon: Facebook,
+    name: "Facebook",
+    url: "https://www.facebook.com/profile.php?id=61569786946519",
+  },
+  {
+    icon: Instagram,
+    name: "Instagram",
+    url: "https://www.instagram.com/atzomx/",
+  },
+  {
+    icon: Phone,
+    name: "WhatsApp",
+    url: "https://wa.me/5219515155328",
+  },
+];
 
 const Footer = () => {
   const t = useTranslations("home.footer");
@@ -42,47 +61,26 @@ const Footer = () => {
           className="payment-icon"
         /> */}
       </div>
-      <div className="bg-theme-black flex flex-col gap-20 text-white m-5 rounded-md px-20 py-10">
+      <div className="bg-theme-black flex flex-col gap-10 text-white m-5 rounded-md px-20 py-10">
         <div className="flex flex-col gap-5 md:flex-row justify-around">
           <div className="flex flex-col gap-2 w-full md:w-1/3">
-            <h1 className="text-xl font-bold">Ubicación</h1>
-            <Link href="/">
-              C. de Los Libres 800-letra B, RUTA INDEPENDENCIA, Centro, 68000
-              Oaxaca de Juárez, Oax.
-            </Link>
+            <h1 className="text-xl font-bold">{t("social_networks")}</h1>
+            {SOCIAL_NETWORKS.map(network => (
+              <span className="flex items-center gap-2" key={network.name}>
+                <network.icon size={24} className="min-w-[24px]" />
+                <a href={network.url} target="_blank" rel="noopener noreferrer">
+                  {network.name}
+                </a>
+              </span>
+            ))}
           </div>
           <div className="flex flex-col gap-2 w-full md:w-1/3">
-            <h1 className="text-xl font-bold">Nuestras Redes Sociales</h1>
-            <span className="flex items-center gap-2">
-              <Facebook size={24} className="min-w-[24px]" />
-              <a
-                href="https://www.facebook.com/profile.php?id=61569786946519"
-                target="_blank"
-                rel="noopener noreferrer">
-                Facebook
-              </a>
-            </span>
-            <span className="flex items-center gap-2">
-              <Instagram size={24} className="min-w-[24px]" />
-              <a
-                href="https://www.instagram.com/atzomx/"
-                target="_blank"
-                rel="noopener noreferrer">
-                Instagram
-              </a>
-            </span>
-            <span className="flex items-center gap-2">
-              <Phone size={24} className="min-w-[24px]" />
-              <a
-                href="https://wa.me/5219515155328"
-                target="_blank"
-                rel="noopener noreferrer">
-                WhatsApp
-              </a>
-            </span>
+            <h1 className="text-xl font-bold">{t("address")}</h1>
+            <p className="max-w-[80%]">{ADDRESS}</p>
           </div>
+
           <div className="flex flex-col gap-2 w-full md:w-1/3">
-            <h1 className="text-xl font-bold">Encuéntranos en</h1>
+            <h1 className="text-xl font-bold">{t("location")}</h1>
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d476.7599133940934!2d-96.7181858!3d17.068773!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85c723cfaf60de6b%3A0xd4660acd5a3a65bf!2sAtzomx%20Coworking!5e0!3m2!1ses-419!2smx!4v1740720931272!5m2!1ses-419!2smx"
               width="100%"
