d0a642e7e798356ce618b9e4105080a1cc0d792a
Marcos Morales Mdz <marcos611morales@gmail.com>
2026-04-27 15:27:47 -0600

    chore: update product images and descriptions in mock data

--- src/mocks/products.ts
+++ src/mocks/products.ts
@@ -8,27 +8,27 @@ import {
 export const ALL_IMAGES: ImageType[] = [
   {
     id: 1,
-    name: "Category 1",
+    name: "Sala de juntas en Atzomx Coworking, con TV 55 pulgadas y pizarrón, para 2 a 6 personas en el Centro de Oaxaca",
     image: "/images/coworking/meeting-room.webp",
   },
   {
-    id: 1,
-    name: "Category 1",
+    id: 2,
+    name: "Latte art preparado con café de especialidad en Atzomx Café, Centro Histórico de Oaxaca",
     image: "/images/coworking/art-latte.webp",
   },
   {
-    id: 1,
-    name: "Category 1",
+    id: 3,
+    name: "Fachada de Atzomx Café y Coworking en C. de Los Libres 800-B, Centro de Oaxaca",
     image: "/images/coworking/atzomx.webp",
   },
   {
-    id: 1,
-    name: "Category 1",
+    id: 4,
+    name: "Sándwich Ibérico con ensalada de la casa, parte del menú de Atzomx",
     image: "/images/coworking/lunch.webp",
   },
   {
-    id: 1,
-    name: "Category 1",
+    id: 5,
+    name: "Escritorio individual con monitor adicional en el coworking de Atzomx Oaxaca",
     image: "/images/coworking/monitor.webp",
   },
 ];
